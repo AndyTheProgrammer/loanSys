e50f5c450b3f3dc89e05914258b6e954b9dd6558
AndyTheProgrammer <59385840+AndyTheProgrammer@users.noreply.github.com>
2021-03-09 02:22:05 +0200

    Changed from old database to updated
    s

--- SQL File/loginsystem.sql
+++ SQL File/loginsystem.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2020 at 03:39 AM
--- Server version: 10.3.15-MariaDB
--- PHP Version: 7.2.19
+-- Generation Time: Mar 09, 2021 at 01:15 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -44,6 +43,45 @@ INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `loan`
+--
+
+CREATE TABLE `loan` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lamount` double NOT NULL,
+  `lpurpose` text NOT NULL,
+  `payable` double NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `loan`
+--
+
+INSERT INTO `loan` (`id`, `user_id`, `fname`, `lamount`, `lpurpose`, `payable`, `status`) VALUES
+(63, 32, 'Wilson', 2500, 'House Loan', 2875, 0),
+(64, 33, 'Batuke', 25000, 'House Loan', 28750, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loan_request`
+--
+
+CREATE TABLE `loan_request` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `lamount` float NOT NULL,
+  `textarea` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -54,16 +92,18 @@ CREATE TABLE `users` (
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(300) DEFAULT NULL,
   `contactno` varchar(11) DEFAULT NULL,
-  `posting_date` timestamp NOT NULL DEFAULT current_timestamp()
+  `posting_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `max_amount` double NOT NULL,
+  `min_amount` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `contactno`, `posting_date`) VALUES
-(9, 'Anuj', 'Kumar', 'demouser@gmail.com', 'Test@123', '21222222', '2020-04-15 18:30:00'),
-(11, 'Anuj', 'Kumar', 'phpgurukulofficial@gmail.com', 'Test@123', '1234567890', '2020-04-15 18:30:00');
+INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `contactno`, `posting_date`, `max_amount`, `min_amount`) VALUES
+(32, 'Wilson', 'simwanza', 'jaysimwanza@gmail.com', 'simwanza12', '0974907421', '2021-03-09 00:53:53', 3000, 1000),
+(33, 'Batuke', 'Lilembalemba', 'blilembalemba@gmail.com', 'batuke', '0976309693', '2021-03-09 00:58:47', 50000, 10000);
 
 --
 -- Indexes for dumped tables
@@ -73,6 +113,19 @@ INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `contactno`, `
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `loan`
+--
+ALTER TABLE `loan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Test` (`user_id`);
+
+--
+-- Indexes for table `loan_request`
+--
+ALTER TABLE `loan_request`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -92,10 +145,32 @@ ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `loan`
+--
+ALTER TABLE `loan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+
+--
+-- AUTO_INCREMENT for table `loan_request`
+--
+ALTER TABLE `loan_request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `loan`
+--
+ALTER TABLE `loan`
+  ADD CONSTRAINT `Test` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
